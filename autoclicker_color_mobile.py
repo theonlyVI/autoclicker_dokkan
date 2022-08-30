588206fcd821a7e489031afa966958bf34884bce
Viet Anh Nguyen <imcpro12345@gmail.com>
2022-08-30 02:37:16 -0300

    added autoclicker color for mobile Added terminating logic

--- autoclicker_color_mobile.py
+++ autoclicker_color_mobile.py
@@ -8,27 +8,24 @@ import signal
 import sys
 
 
-device = zxtouch("192.168.137.202")
-# device = zxtouch("10.0.0.230")
-
-
-# device.touch(TOUCH_DOWN, 5, 400, 400)
-# device.show_toast(TOAST_MESSAGE, "this is test program", 2)
-# time.sleep(1)
-# print(device.get_screen_size())
-
-# device.touch(TOUCH_DOWN, 1, 400, 1150)
-# device.touch(TOUCH_UP, 1, 400, 1150)
-
-def main():
-    print(device.image_match("/var/mobile/Library/ZXTouch/scripts/img/IMG_9125.JPG", 0.95, 4, 1))
-    print(device.search_color((611, 1483, 200, 300), 210, 255, 220, 255, 220, 255))
-    # print(device.pick_color(312, 1195))
-    # print(device.search_color((360, 1024, 300, 200), 200, 255, 100, 255, 0, 50))
-
+device = zxtouch("127.0.0.1")
     
-    # sta
-    while True:
+def compare_color(c1: dict, c2: tuple):
+    if int(c1["red"]) == c2[0] and int(c1["green"]) == c2[1]:
+        return True
+    return False
+
+def real_touch(x: int, y: int):
+    device.touch(TOUCH_DOWN, 1, x, y)
+    device.touch(TOUCH_UP, 1, x, y)
+    # time.sleep(0.5)
+
+def handler(sig: int, frame):
+    device.disconnect()
+    print("\nExited with Ctrl+C")
+    sys.exit(0)
+
+while True:
         signal.signal(signal.SIGINT, handler)
         # sta
         if compare_color(device.pick_color(530, 1017)[1], (211, 97, 58)):
@@ -51,21 +48,3 @@ def main():
         elif compare_color(device.pick_color(762, 1592)[1], (255,239,238)):
             device.show_toast(TOAST_MESSAGE, "The program has been terminated", 2)
         real_touch(50, 1700)
-
-def compare_color(c1: dict, c2: tuple):
-    if int(c1["red"]) == c2[0] and int(c1["green"]) == c2[1]:
-        return True
-    return False
-
-def real_touch(x: int, y: int):
-    device.touch(TOUCH_DOWN, 1, x, y)
-    device.touch(TOUCH_UP, 1, x, y)
-    # time.sleep(0.5)
-
-def handler(sig: int, frame):
-    device.disconnect()
-    print("\nExited with Ctrl+C")
-    sys.exit(0)
-
-if __name__ == "__main__":
-    main()
